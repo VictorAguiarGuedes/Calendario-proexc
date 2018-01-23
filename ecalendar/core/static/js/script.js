0d6a3dd52312dcd8ff11f6542f65797e86cf9407
Pamella <pbsf2@cin.ufpe.br>
2018-01-23 19:56:48 -0300

    criação da estrutura do modal de um evento

---
+++ ecalendar/core/static/js/script.js
@@ -0,0 +1,12 @@
+//event modal
+var me = document.getElementById("id-modal-event");
+
+me.addEventListener("click", function(e) {
+	if (e.target === this) {
+		closeEventModal();
+	}
+});
+
+function closeEventModal() {
+	me.style.display = "none";
+}
